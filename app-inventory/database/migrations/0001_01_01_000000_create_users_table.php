524fd791ed395bd87061aaa5cbb1611b4c8f0cd0
engfabiodesalvi <engfabiodesalvi@gmail.com>
2026-03-18 16:14:05 -0300

    feat: add role column to users table and adjust Docker environment with Node
    Adds the role field with a default value to the users table.
    Creates a dedicated Docker container for Node.
    Updates the PHP Dockerfile.
    Adjusts .env variables for MySQL, Redis, and containerized services.

--- app-inventory/database/migrations/0001_01_01_000000_create_users_table.php
+++ app-inventory/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            //  Adding the role field
+            $table->string('role')->default('operador'); // role -> função
+
             $table->rememberToken();
             $table->timestamps();
         });
