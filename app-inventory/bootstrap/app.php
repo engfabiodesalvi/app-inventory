45252130100fe540a823d2354ab71c739ba1bfad
engfabiodesalvi <engfabiodesalvi@gmail.com>
2026-03-19 20:09:44 -0300

    feat: Create the role middleware

--- app-inventory/bootstrap/app.php
+++ app-inventory/bootstrap/app.php
@@ -13,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         # TODO - Day 4 - Authorization Middleware (Roles)        
         $middleware->alias([
-            'role' => \App\Http\Middleware\CheckRole::class ,
+            //'role' => \App\Http\Middleware\CheckRole::class ,
+            'role' => \App\Http\Middleware\RoleMiddleware::class ,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
